b913a9b6a7f077eee883213fabb7e7d858ef43d0
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-02-21 21:32:03 +0100

    reverted to not ignoring images

---
+++ src/test/kotlin/NegativeKtTest.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class NegativeKtTest {
+
+    @Test
+    fun main() {
+    }
+
+    @Test
+    fun negateColours() {
+    }
+}
